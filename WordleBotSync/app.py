0363c3a090490e89bd056d7a61dd415943a0fa6c
Matt Perry <matthewperrydev@gmail.com>
2022-05-23 07:47:19 -0400

    Attempting a second time

--- WordleBotSync/app.py
+++ WordleBotSync/app.py
@@ -1,6 +1,17 @@
 import requests
 import boto3
-
+import json
 
 def lambda_handler(event, context):
-	pass
+    ssm = boto3.client('ssm')
+    GUILD_ID = ssm.get_parameter(Name='guild-id', WithDecryption=False)['Parameter']['Value']
+    BOT_TOKEN = ssm.get_parameter(Name='bot-token', WithDecryption=False)['Parameter']['Value']
+    APP_ID = ssm.get_parameter(Name='app-id', WithDecryption=False)['Parameter']['Value']
+    
+    url = f'https://discord.com/api/v8/applications/{APP_ID}/guilds/{GUILD_ID}/commands'
+
+    json = json.loads(open('commands.json','r').read())
+
+    response = requests.post(url, headers={
+        'Authorization': f'Bot {BOT_TOKEN}'
+    }, json=json)
